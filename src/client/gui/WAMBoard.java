7474c03fc3d6be8ded329b98503fde1ca54af739
ms1450 <ms1450@rit.edu>
2019-04-09 09:44:23 -0400

    Client Change

--- src/client/gui/WAMBoard.java
+++ src/client/gui/WAMBoard.java
@@ -3,4 +3,16 @@ package client.gui;
 public class WAMBoard {
     public void error(String arguments) {
     }
+
+    public void gameWon() {
+    }
+
+    public void gameLost() {
+    }
+
+    public void gameTied() {
+    }
+
+    public void close() {
+    }
 }
--- src/client/gui/WAMClient.java
+++ src/client/gui/WAMClient.java
@@ -5,6 +5,7 @@ import common.WAMProtocol;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import static common.WAMProtocol.*;
@@ -66,10 +67,83 @@ public class WAMClient {
         new Thread(() -> this.run()).start();
     }
 
+    public void gameWon() {
+        WAMClient.dPrint( '!' + GAME_WON );
 
-
-    private void run() {
+        dPrint( "You won! Yay!" );
+        this.board.gameWon();
+        this.stop();
     }
+
+    public void gameLost() {
+        WAMClient.dPrint( '!' + GAME_LOST );
+        dPrint( "You lost! Boo!" );
+        this.board.gameLost();
+        this.stop();
+    }
+
+    public void gameTied() {
+        WAMClient.dPrint( '!' + GAME_TIED );
+        dPrint( "You tied! Meh!" );
+        this.board.gameTied();
+        this.stop();
+    }
+
+    public void close() {
+        try {
+            this.clientSocket.close();
+        }
+        catch( IOException ioe ) {
+            // squash
+        }
+        this.board.close();
+    }
+
+//    private void run() {
+//        while (this.goodToGo()) {
+//            try {
+//                String request = this.networkIn.next();
+//                String arguments = this.networkIn.nextLine().trim();
+//                ConnectFourNetworkClient.dPrint( "Net message in = \"" + request + '"' );
+//
+//                switch ( request ) {
+//                    case MAKE_MOVE:
+//                        makeMove();
+//                        break;
+//                    case MOVE_MADE:
+//                        moveMade( arguments );
+//                        break;
+//                    case GAME_WON:
+//                        gameWon();
+//                        break;
+//                    case GAME_LOST:
+//                        gameLost();
+//                        break;
+//                    case GAME_TIED:
+//                        gameTied();
+//                        break;
+//                    case ERROR:
+//                        error( arguments );
+//                        break;
+//                    default:
+//                        System.err.println("Unrecognized request: " + request);
+//                        this.stop();
+//                        break;
+//                }
+//            }
+//            catch( NoSuchElementException nse ) {
+//                // Looks like the connection shut down.
+//                this.error( "Lost connection to server." );
+//                this.stop();
+//            }
+//            catch( Exception e ) {
+//                this.error( e.getMessage() + '?' );
+//                this.stop();
+//            }
+//        }
+//        this.close();
+//    }
+//    }
 
 
 }
